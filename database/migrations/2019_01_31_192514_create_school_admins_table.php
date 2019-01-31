a11fa52374df4c7029c36ced7dea7c9d1da64dc1
MICHAEL KABUTEY <mkabutey@MICHAELs-MacBook-Pro.local>
2019-01-31 19:29:03 +0000

    Models Created

--- database/migrations/2019_01_31_192514_create_school_admins_table.php
+++ database/migrations/2019_01_31_192514_create_school_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateSchoolAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('school_admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('school_admins');
     }
 }
